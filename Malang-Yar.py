5c83f03493de01d6f5315afdf8d8f3088a425a41
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-06-01 17:06:12 +0500

    Create Malang-Yar.py
    git clone https://github.com/malangyar/SAHIL-PRINCE-1.git
    cd SAHIL-PRINCE-1
    python3 Malang-Yar.py

---
+++ Malang-Yar.py
@@ -0,0 +1,14 @@
+import os
+os.system('git pull')
+os.system('termux-setup-storage')
+print("\033[1;32m   WELCOME TO MY TOLL")
+print("\033[1;32m   JALNE WALO JALNA NHI")
+import kkkkkkkk
+Main()
+
+
+
+
+
+
+
